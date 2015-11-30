028d8338617e4a1ae9190324e029ff3d33c8a7bf
andrius <andrius@cambridgecoding.com>
2015-11-30 19:45:29 +0000

    gh-pages for hosting web assets

--- models/accounts.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var Accounts = new Schema({
-  hat_token: String,
-  last_events_update: String
-});
-
-module.exports = mongoose.model('Accounts', Accounts);
